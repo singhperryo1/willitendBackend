287914ae1cd9733eed71da12079b5273d859ba04
Gagandeep Singh <singh@Gagandeeps-MacBook-Pro.local>
2021-04-08 19:20:39 -0700

    setting up initial files

---
+++ src/main/java/com/willitend/backend/willitendBackend/WillitendBackendApplication.java
@@ -0,0 +1,13 @@
+package com.willitend.backend.willitendBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WillitendBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WillitendBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/willitend/backend/willitendBackend/WillitendBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willitend.backend.willitendBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WillitendBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
