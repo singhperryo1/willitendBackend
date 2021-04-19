8ee80319bc9525b6724e975801f038f76998966c
JaredTruston <jaredtruston@gmail.com>
2021-04-18 20:52:00 -0700

    Added Repository and Controller for Experience

--- src/main/java/com/willitend/backend/willitendBackend/Experience.java
+++ src/main/java/com/willitend/backend/willitendBackend/Experience.java
@@ -1,13 +1,18 @@
 package com.willitend.backend.willitendBackend;
+import java.util.Date;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "experiences")
 public class Experience {
+	// Fields
 	@Id
-	private String date;
+	@Column(name = "date")
+	private Date date;
 	
 	@Id
+	@Column(name = "email")
 	private String email;
 	
 	@Column(name = "userName")
@@ -24,4 +29,44 @@ public class Experience {
 	
 	@Column(name = "vaccExp")
 	private String vaccExp;
+	
+	// Constructor for Experience
+	public Experience(Date date, String email, String userName, String password, String site, String title, String vaccExp) {
+		this.date = date;
+		this.email = email;
+		this.userName = userName;
+		this.password = password;
+		this.site = site;
+		this.title = title;
+		this.vaccExp = vaccExp;
+	}
+	
+	// Getter Methods
+	public Date getDate() {
+		return this.date;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public String getUserName() {
+		return this.userName;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+	
+	public String getSite() {
+		return this.site;
+	}
+	
+	public String getTitle() {
+		return this.title;
+	}
+	
+	public String getVaccExp() {
+		return this.vaccExp;
+	}
 }
