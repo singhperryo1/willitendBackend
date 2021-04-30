0b63cf4064a7b984a424cbc4d29ba09d973bd706
lihing1994 <li_hing@student.smc.edu>
2021-04-30 16:11:14 -0700

    Update NewsletterController.java

--- src/main/java/com/willitend/backend/willitendBackend/controller/NewsletterController.java
+++ src/main/java/com/willitend/backend/willitendBackend/controller/NewsletterController.java
@@ -4,27 +4,60 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.willitend.backend.willitendBackend.model.Newsletter;
-import com.willitend.backend.willitendBackend.repository.NewsletterRepository;
+import com.willitend.backend.willitendBackend.model.User;
+import com.willitend.backend.willitendBackend.repository.UserRepository;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
-@RequestMapping("/newsletter")
-public class NewsletterController {
+@RequestMapping("/NewsLetter")
+public class NewsLetterController {
 	
-	@Autowired 
-	NewsletterRepository newsletterRepository; 
+	@Autowired
+	UserRepository nlRepository; 
 	
-	@PostMapping ("/create")
-	public ResponseEntity<Newsletter> createNewsletter (@RequestBody Newsletter newsletter) {
+	@GetMapping("/getNesLetter")
+	public ResponseEntity<NewsLetter> getNewsLetter(@PathVariable("email") String email) {
 		try {
-			Newsletter _newsletter = newsletterRepository.save(new Newsletter(newsletter.getEmail(), newsletter.getState()));
-			return new ResponseEntity<>(null, HttpStatus.CREATED); 
+			NewLetter nl = userRepository.findByUsername(email); 
+			
+			if (user == null) {
+				return new ResponseEntity<>(null, HttpStatus.NO_CONTENT); 
+			}
+
+			return new ResponseEntity<>(nl, HttpStatus.OK); 
+			
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	@GetMapping("/getAllNewsLetter")
+	public ResponseEntity<List<NewsLetter>> getAllNewsLetter() {
+		try {
+			List<NewsLetter> NewsLetter = new ArrayList<>(); 
+			
+			stateinfoRepository.findAll().forEach(NewsLetter::add);
+			
+			if (NewsLetter.isEmpty()) {
+				return new ResponseEntity<>(HttpStatus.NO_CONTENT); 
+			}
+			
+			return new ResponseEntity<>(NewsLetter, HttpStatus.OK); 
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	@PostMapping("/creatNewsLetter")
+	public ResponseEntity<NewsLetter> creatNewsLetter(@RequestBody NewsLetter nl) {
+		try {
+			NewsLetter nl = nlRepository.save(new NewsLetter(nl.getUserEmail(),nl.getUserState()));
+			return new ResponseEntity<>(nl,  HttpStatus.CREATED); 
 		} catch (Exception e) {
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR); 
 		}
